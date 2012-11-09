4f2b164bdc1e0411149779aa1501438d31a566fe
unknown <bloov@tut.by>
2012-11-10 01:15:21 +0300

    Add MainMenuScene

--- source/Project87/Scenes/Project87.Scenes.MainMenuScene.pas
+++ source/Project87/Scenes/Project87.Scenes.MainMenuScene.pas
@@ -2,6 +2,31 @@ unit Project87.Scenes.MainMenuScene;
 
 interface
 
+uses
+  QCore.Input,
+  QGame.Scene,
+  Strope.Math;
+
+type
+  TMainMenuScene = class sealed (TScene)
+    strict private
+    public
+      constructor Create(const AName: string);
+      destructor Destroy; override;
+  end;
+
 implementation
+
+{$REGION '  TMainMenuScene  '}
+constructor TMainMenuScene.Create(const AName: string);
+begin
+  inherited Create(AName);
+end;
+
+destructor TMainMenuScene.Destroy;
+begin
+  inherited;
+end;
+{$ENDREGION}
 
 end.
